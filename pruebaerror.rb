3cd851d49f19f84d4280a5852e4b5ce332b5ed6a
codeamac04 <codeamac04@gmail.com>
2015-09-09 20:06:18 -0500

    pruebas en la consola

---
+++ pruebaerror.rb
@@ -0,0 +1,27 @@
+Last login: Wed Sep  9 17:20:09 on ttys000
+CodeaCamp04s-Mac-mini:~ codea_mac_04$ irb
+irb(main):001:0> 6 + 3
+=> 9
+irb(main):002:0> 9 - 5
+=> 4
+irb(main):003:0> "jonathan"
+=> "jonathan"
+irb(main):004:0> name = "jonathan"
+=> "jonathan"
+irb(main):005:0> name
+=> "jonathan"
+irb(main):006:0> [ 1, 2, 3..4, 'five' ].inspect
+=> "[1, 2, 3..4, \"five\"]"
+irb(main):007:0> 8.5.object_id
+=> 74309393851613186
+irb(main):008:0> name.object_id
+=> 70201387539880
+irb(main):009:0> module math
+irb(main):010:1> sqrt 7690939204
+irb(main):011:1> genr x = sqrt(7690939204)
+irb(main):012:1> math
+irb(main):013:1> genr x = sqrt(7690939204)
+irb(main):014:1> di_hola 
+irb(main):015:1> di_hola = "Javier"
+irb(main):016:1> puts hola"#{di_hola}"
+irb(main):017:1> 
